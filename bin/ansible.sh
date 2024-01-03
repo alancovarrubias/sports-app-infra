0c4b74e5cf643c12c3bbda394d47ded2bd5a53ef
Alan Covarrubias <alan.cov@icloud.com>
2024-01-03 00:08:52 -0800

    add tag to ansible script

--- bin/ansible.sh
+++ bin/ansible.sh
@@ -1,2 +1,2 @@
 cd ansible
-ansible-playbook --vault-password-file ~/.vault_pass.txt --inventory $DROPLET_IP, -e target_host_ip=$DROPLET_IP --private-key ~/.ssh/id_rsa --user root setup_server.yml
+ansible-playbook --vault-password-file ~/.vault_pass.txt --inventory $DROPLET_IP, -e target_host_ip=$DROPLET_IP --private-key ~/.ssh/id_rsa --user root --tags ${1:-all} setup_server.yml
