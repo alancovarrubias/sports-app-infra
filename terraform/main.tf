5daba99e66a462d2d5afb6727027aacbc43adf44
Alan Covarrubias <alan.cov@icloud.com>
2024-01-03 22:50:01 -0800

    add remote state for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "sports-app-buck"
+    key    = "sports-app/state.tfstate"
+    region = "us-west-1"
+  }
+}
+
 variable "do_token" {}
 variable "public_ssh_key" {}
 variable "private_ssh_key" {}
