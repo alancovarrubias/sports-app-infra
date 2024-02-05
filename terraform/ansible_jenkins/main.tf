2897947b3cd66ed160d2319031752da14f19719d
Alan Covarrubias <alan.cov@icloud.com>
2024-02-04 21:16:06 -0800

    refactor ansible_jenkins server creation into module

--- terraform/ansible_jenkins/main.tf
+++ terraform/ansible_jenkins/main.tf
@@ -1,47 +1,40 @@
-resource "digitalocean_ssh_key" "default" {
-  name       = "SSH Key"
-  public_key = file(var.public_ssh_key)
+module "ansible_server" {
+  source       = "../modules/do_droplet"
+  do_token     = var.do_token
+  droplet_name = "ansible-server"
+  droplet_size = "s-1vcpu-1gb"
 }
 
-resource "digitalocean_droplet" "ansible_server" {
-  image    = "ubuntu-22-04-x64"
-  name     = "ansible-server"
-  region   = "sfo2"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
-}
-
-resource "digitalocean_droplet" "jenkins_server" {
-  image    = "ubuntu-22-04-x64"
-  name     = "jenkins-server"
-  region   = "sfo2"
-  size     = "s-2vcpu-4gb"
-  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
+module "jenkins_server" {
+  source       = "../modules/do_droplet"
+  do_token     = var.do_token
+  droplet_name = "jenkins-server"
+  droplet_size = "s-2vcpu-4gb"
 }
 
 resource "null_resource" "configure_jenkins_server" {
   triggers = {
-    trigger = digitalocean_droplet.jenkins_server.ipv4_address
+    trigger = module.ansible_server.ip_address
   }
   provisioner "local-exec" {
     working_dir = "../../ansible"
     command = format(
       "ansible-playbook --inventory %s, -e ansible_ip=%s --extra-vars @extra_vars.yml --skip-tags plugins setup_jenkins.yml",
-      digitalocean_droplet.jenkins_server.ipv4_address,
-      digitalocean_droplet.ansible_server.ipv4_address,
+      module.jenkins_server.ip_address,
+      module.ansible_server.ip_address,
     )
   }
 }
 
 resource "null_resource" "configure_ansible_server" {
   triggers = {
-    trigger = digitalocean_droplet.ansible_server.ipv4_address
+    trigger = module.ansible_server.ip_address
   }
   provisioner "local-exec" {
     working_dir = "../../ansible"
     command = format(
       "ansible-playbook --inventory %s, --extra-vars @extra_vars.yml setup_ansible.yml",
-      digitalocean_droplet.ansible_server.ipv4_address,
+      module.ansible_server.ip_address,
     )
   }
 }
