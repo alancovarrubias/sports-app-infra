460e07c244cd1047261739bc6729e3897d1c1157
Alan Covarrubias <aecovarru@gmail.com>
2024-02-05 18:57:44 -0800

    create record from existing domain

--- terraform/sports_app/dns.tf
+++ terraform/sports_app/dns.tf
@@ -1,10 +1,5 @@
-resource "digitalocean_domain" "default" {
-  name       = var.domain_name
-  ip_address = module.sports-app.ip_address
-}
-
 resource "digitalocean_record" "www" {
-  domain = digitalocean_domain.default.id
+  domain = var.domain_name
   type   = "A"
   name   = "www"
   value  = module.sports-app.ip_address
