89a991093466cf8f2b047556135dea9034ec3963
Alan Covarrubias <aecovarru@gmail.com>
2024-02-10 23:24:26 -0800

    add run tunnel logic

--- ansible/setup_sports_app_dev/templates/tunnel.sh
+++ ansible/setup_sports_app_dev/templates/tunnel.sh
@@ -1,1 +1,2 @@
-ssh -L 1234:localhost:1234 -L 3000:localhost:3000 {{ user_name }}@{{ inventory_hostname }} -fN
+#!/bin/bash
+ssh -fN -L 1234:localhost:1234 -L 3000:localhost:3000 {{ user_name }}@{{ inventory_hostname }}
