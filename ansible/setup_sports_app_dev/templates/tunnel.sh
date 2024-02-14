5206870bc757769e162b903a7356f092572abf31
Alan Covarrubias <aecovarru@gmail.com>
2024-02-13 23:29:13 -0800

    restore database

--- ansible/setup_sports_app_dev/templates/tunnel.sh
+++ ansible/setup_sports_app_dev/templates/tunnel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -fN -L 1234:localhost:1234 -L 4000:localhost:4000 -L 3000:localhost:3000 {{ user_name }}@{{ inventory_hostname }}
+ssh -fN -L 1234:localhost:1234 -L 4000:localhost:4000 -L 3000:localhost:3000 -L 5000:localhost:5000 {{ user_name }}@{{ inventory_hostname }}
