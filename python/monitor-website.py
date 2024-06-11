0928ced989b03206b05945c8c1125a54a1a2f4e3
Alan Covarrubias <aecovarru@gmail.com>
2024-06-10 21:09:15 -0700

    wip

--- python/monitor-website.py
+++ python/monitor-website.py
@@ -1,11 +1,14 @@
 import requests
 from send_email import SendEmail
 
-# response = requests.get('http://fake.com:8080')
-
-# if response.status_code == 200:
-#     print('Application fine')
-# else:
-print('Application down')
 send_email = SendEmail()
-send_email.run('Subject', 'Body')
+try:
+    response = requests.get('http://fake.com:8080')
+    if response.status_code == 200:
+        print('Application fine')
+    else:
+        print('Application down')
+        send_email.run('SITE DOWN', f'Application returned {response.status_code}')
+except Exception as ex:
+    print(ex)
+    send_email.run('Connection error', 'Application not accessible')
