c2c76790aaafa95173c3384d90669b80fbb81317
Alan Covarrubias <aecovarru@gmail.com>
2024-06-20 23:57:31 -0700

    wip

--- python/monitor-website.py
+++ python/monitor-website.py
@@ -1,9 +1,21 @@
 import requests
 from send_email import SendEmail
+import paramiko
 
 send_email = SendEmail()
+def read_server():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname='138.68.16.95', username='alancovarrubias', key_filename='/Users/alancovarrubias/.ssh/id_rsa')
+    stdin, stdout, stderr = ssh.exec_command('docker ps')
+    print(stdout.readlines())
+    ssh.close()
+
+read_server()
 try:
-    response = requests.get('http://fake.com:8080')
+    print('Attempting')
+    response = requests.get('http://138.68.16.95:3000')
+    print('Completed')
     if response.status_code == 200:
         print('Application fine')
     else:
