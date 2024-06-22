44c5788a6d9c3f3a45310a4989ecd02a8f9d5d92
Alan Covarrubias <aecovarru@gmail.com>
2024-06-21 23:53:54 -0700

    update jenkins plugin file

--- python/monitor-website.py
+++ python/monitor-website.py
@@ -1,11 +1,26 @@
 import requests
 from send_email import SendEmail
+import paramiko
 
-# response = requests.get('http://fake.com:8080')
-
-# if response.status_code == 200:
-#     print('Application fine')
-# else:
-print('Application down')
 send_email = SendEmail()
-send_email.run('Subject', 'Body')
+def read_server():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname='138.68.16.95', username='alancovarrubias', key_filename='/Users/alancovarrubias/.ssh/id_rsa')
+    stdin, stdout, stderr = ssh.exec_command('docker ps')
+    print(stdout.readlines())
+    ssh.close()
+
+read_server()
+try:
+    print('Attempting')
+    response = requests.get('http://138.68.16.95:3000')
+    print('Completed')
+    if response.status_code == 200:
+        print('Application fine')
+    else:
+        print('Application down')
+        send_email.run('SITE DOWN', f'Application returned {response.status_code}')
+except Exception as ex:
+    print(ex)
+    send_email.run('Connection error', 'Application not accessible')
