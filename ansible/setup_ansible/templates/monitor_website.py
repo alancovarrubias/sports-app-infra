528c8066c8aff394e2e609f128d4b1f9401aeb27
Alan Covarrubias <aecovarru@gmail.com>
2024-10-02 21:40:19 -0700

    monitor worker

--- ansible/setup_ansible/templates/monitor_website.py
+++ ansible/setup_ansible/templates/monitor_website.py
@@ -4,31 +4,40 @@ import paramiko
 import schedule
 
 send_email = SendEmail()
-def restart_server():
+def restart_server(ip):
     print('Restarting server')
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname="{{ web_ip }}", username="{{ user_name }}", key_filename="/home/{{ user_name }}/.ssh/id_rsa")
+    ssh.connect(hostname=f"{ ip }", username="{{ user_name }}", key_filename="/home/{{ user_name }}/.ssh/id_rsa")
     stdin, stdout, stderr = ssh.exec_command("cd {{ repo_name }} && docker-compose up -d")
     print(stdout.readlines())
     ssh.close()
 
-def monitor_website():
+def monitor_website(ip, port, path):
     try:
-        response = requests.get("http://{{ web_ip }}")
+        response = requests.get(f"http://{ ip }:{ port }{path}")
         if response.status_code == 200:
             print('Application fine')
         else:
             print('Application down')
             send_email.run('SITE DOWN', f'Application returned {response.status_code}')
-            restart_server()
+            restart_server(ip)
     except Exception as ex:
         msg = 'Application not accessible'
         print(msg)
         send_email.run('Connection error', msg)
-        restart_server()
+        restart_server(ip)
 
-schedule.every(5).minutes.do(monitor_website)
+def monitor():
+    ips = ["{{ web_ip }}", "{{ worker_ip }}"]
+    ports = [80, 5000]
+    paths = ["", "/health"]
+    for index, ip in enumerate(ips):
+        port = ports[index]
+        path = paths[index]
+        monitor_website(ip, port, path)
+
+schedule.every(5).minutes.do(monitor)
 
 while True:
     schedule.run_pending()
