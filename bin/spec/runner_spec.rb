24b5dc59d811832a2a9a73ab86237681eb20734e
Alan Covarrubias <aecovarru@gmail.com>
2024-10-05 00:56:13 -0700

    wip

--- bin/spec/runner_spec.rb
+++ bin/spec/runner_spec.rb
@@ -1,8 +1,20 @@
 require 'runner'
 # spec/cli_spec.rb
 RSpec.describe 'Runner' do
-  it 'greets the user by name' do
-    expect(Runner.run(command: 'run',
-                      module: 'dump')).to eq('cd ansible && ansible-playbook --inventory 143.110.207.14, --extra-vars @extra_vars.yml --skip-tags skip --tags  database_cmd.yml')
+  let(:inventory) { '143.110.207.14' }
+  before do
+    @command = Runner.run(command: 'run', module: mod, inventory: inventory)
+  end
+  describe 'jenkins command' do
+    let(:mod) { 'jenkins' }
+    it 'greets the user by name' do
+      expect(@command).to eq("cd ansible && ansible-playbook --inventory #{inventory}, --extra-vars @extra_vars.yml --skip-tags skip  setup_jenkins.yml")
+    end
+  end
+  describe 'dump command' do
+    let(:mod) { 'dump' }
+    it 'greets the user by name' do
+      expect(@command).to eq("cd ansible && ansible-playbook --inventory #{inventory}, --extra-vars @extra_vars.yml --skip-tags skip --tags  database_cmd.yml")
+    end
   end
 end
