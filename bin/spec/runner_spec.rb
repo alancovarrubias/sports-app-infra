70660f0a5dd19cf8ccca6e1713fa46c3372ee7c1
Alan Covarrubias <aecovarru@gmail.com>
2024-10-05 01:01:13 -0700

    wip

--- bin/spec/runner_spec.rb
+++ bin/spec/runner_spec.rb
@@ -3,18 +3,31 @@ require 'runner'
 RSpec.describe 'Runner' do
   let(:inventory) { '143.110.207.14' }
   before do
-    @command = Runner.run(command: 'run', module: mod, inventory: inventory)
+    @command = Runner.run(command: command, module: mod, inventory: inventory)
   end
-  describe 'jenkins command' do
-    let(:mod) { 'jenkins' }
-    it 'greets the user by name' do
-      expect(@command).to eq("cd ansible && ansible-playbook --inventory #{inventory}, --extra-vars @extra_vars.yml --skip-tags skip  setup_jenkins.yml")
+  describe 'run command' do
+    let(:command) { 'run' }
+    describe 'jenkins command' do
+      let(:mod) { 'jenkins' }
+      it 'greets the user by name' do
+        expect(@command).to eq("cd ansible && ansible-playbook --inventory #{inventory}, --extra-vars @extra_vars.yml --skip-tags skip  setup_jenkins.yml")
+      end
+    end
+    describe 'dump command' do
+      let(:mod) { 'dump' }
+      it 'greets the user by name' do
+        expect(@command).to eq("cd ansible && ansible-playbook --inventory #{inventory}, --extra-vars @extra_vars.yml --skip-tags skip --tags  database_cmd.yml")
+      end
     end
   end
-  describe 'dump command' do
-    let(:mod) { 'dump' }
-    it 'greets the user by name' do
-      expect(@command).to eq("cd ansible && ansible-playbook --inventory #{inventory}, --extra-vars @extra_vars.yml --skip-tags skip --tags  database_cmd.yml")
+
+  describe 'apply command' do
+    let(:command) { 'apply' }
+    describe 'jenkins command' do
+      let(:mod) { 'dev' }
+      it 'greets the user by name' do
+        expect(@command).to eq('terraform -chdir=$(pwd)/terraform/dev apply -var-file=$(pwd)/terraform/terraform.tfvars --auto-approve')
+      end
     end
   end
 end
