bdf607af36453e4865322a254a6e9a6c8fad70af
Alan Covarrubias <aecovarru@gmail.com>
2025-01-06 00:47:29 -0800

    add firewall to stage

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -5,4 +5,14 @@ terraform {
     key    = "stage/state.tfstate"
     region = "us-west-1"
   }
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
 }
