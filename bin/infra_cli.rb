133d8cba4b59d6047cd1e297b3ba32fc10bb69c4
Alan Covarrubias <aecovarru@gmail.com>
2025-01-17 19:06:06 -0800

    refactor cli

--- bin/infra_cli.rb
+++ bin/infra_cli.rb
@@ -4,49 +4,32 @@ require_relative 'config/zeitwerk'
 require_relative 'config/bundler'
 
 options = {}
+COMMANDS = [
+  [:command, '-c', '--command COMMAND', 'Specify command'],
+  [:env, '-e', '--env ENV', 'Specify env'],
+  [:module, '-m', '--module MODULE', 'Specify module'],
+  [:tags, '--tags TAGS', 'Specify tags'],
+  [:token, '--token TOKEN', 'Specify TOKEN'],
+  [:inventory, '-i', '--inventory INVENTORY', 'Specify inventory'],
+  [:web_ip, '--web_ip WEB_IP', 'Specify web ip'],
+  [:worker_ip, '--worker_ip WORKER_IP', 'Specify worker ip'],
+  [:domain_name, '-d', '--domain DOMAIN_NAME', 'Specify web domain name']
+].freeze
 OptionParser.new do |opts|
   opts.banner = 'Usage: ansible.rb [options]'
 
-  opts.on('-c', '--command COMMAND', 'Specify command') do |cmd|
-    options[:command] = cmd
-  end
-
-  opts.on('-e', '--env ENV', 'Specify env') do |env|
-    options[:env] = env
-  end
-
-  opts.on('-m', '--module MODULE', 'Specify module') do |mod|
-    options[:module] = mod
-  end
-
-  opts.on('--tags TAGS', 'Specify tags') do |tags|
-    options[:tags] = tags.split(',')
-  end
-
-  opts.on('--token TOKEN', 'Specify TOKEN') do |token|
-    options[:token] = token
-  end
-
-  opts.on('-i', '--inventory INVENTORY', 'Specify inventory') do |inventory|
-    options[:inventory] = inventory
-  end
-
-  opts.on('--web_ip WEB_IP', 'Specify web ip') do |web_ip|
-    options[:web_ip] = web_ip
-  end
-
-  opts.on('--worker_ip WORKER_IP', 'Specify worker ip') do |worker_ip|
-    options[:worker_ip] = worker_ip
-  end
-
-  opts.on('-d', '--domain DOMAIN_NAME', 'Specify web domain name') do |domain_name|
-    options[:domain_name] = domain_name
+  COMMANDS.each do |command|
+    key, *args = command
+    opts.on(*args) do |cmd|
+      options[key] = cmd
+    end
   end
 end.parse!
 
 options[:web_ip] ||= ENV['WEB_IP']
 options[:worker_ip] ||= ENV['WORKER_IP']
 options[:token] ||= ENV['DO_TOKEN']
+options[:tags] = options[:tags].split(',') if options[:tags]
 
 def get_runner(command)
   case command
