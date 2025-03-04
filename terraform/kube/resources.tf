46f665b54bbf2ce31428ebf25e41b352ea0f7a37
Alan Covarrubias <aecovarru@gmail.com>
2025-03-03 23:32:57 -0800

    install minikube

--- terraform/kube/resources.tf
+++ terraform/kube/resources.tf
@@ -2,6 +2,6 @@ module "sports_app_kube" {
   source       = "../modules/do_droplet"
   do_token     = var.do_token
   droplet_name = "sports-app-kube"
-  droplet_size = "s-2vcpu-2gb"
+  droplet_size = "s-2vcpu-4gb"
   args         = "-m kube"
 }
