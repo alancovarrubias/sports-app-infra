f905aacd17fd3c5f9ccadde67b93684ad07e83c8
Alan Covarrubias <aecovarru@gmail.com>
2025-11-24 01:36:53 -0800

    Refactor cli

--- bin/infra_cli.rb
+++ bin/infra_cli.rb
@@ -3,59 +3,54 @@ require 'optparse'
 require_relative 'config/zeitwerk'
 require_relative 'config/bundler'
 
-options = {}
-COMMANDS = [
-  [:command, '-c', '--command COMMAND', 'Specify command'],
-  [:env, '-e', '--env ENV', 'Specify env'],
-  [:module, '-m', '--module MODULE', 'Specify module'],
-  [:tags, '--tags TAGS', 'Specify tags'],
-  [:kube_id, '-k', '--kube_id KUBE_ID', 'Specify kube cluster id'],
-  [:token, '--token TOKEN', 'Specify TOKEN'],
-  [:inventory, '-i', '--inventory INVENTORY', 'Specify inventory'],
-  [:web_ip, '--web_ip WEB_IP', 'Specify web ip'],
-  [:worker_ip, '--worker_ip WORKER_IP', 'Specify worker ip'],
-  [:domain_name, '-d', '--domain DOMAIN_NAME', 'Specify web domain name']
-].freeze
-OptionParser.new do |opts|
-  opts.banner = 'Usage: ansible.rb [options]'
+class InfraCLI
+  OPTIONS = {
+    command: ['-c', '--command COMMAND', 'Specify command'],
+    module: ['-m', '--module MODULE', 'Specify module'],
+    tags: ['--tags TAGS', 'Specify tags']
+  }.freeze
+  ANSIBLE_RUNNER = 'ansible'.freeze
+  TERRAFORM_RUNNER = 'terraform'.freeze
+  APPLY_COMMAND = 'apply'.freeze
+  DESTROY_COMMAND = 'destroy'.freeze
+  RUN_COMMAND = 'run'.freeze
 
-  COMMANDS.each do |command|
-    key, *args = command
-    opts.on(*args) do |cmd|
-      options[key] = cmd
+  def initialize
+    @options = {}
+    OptionParser.new do |opts|
+      OPTIONS.each do |key, args|
+        opts.on(*args) do |cmd|
+          @options[key] = cmd
+        end
+      end
+    end.parse!
+    @options[:tags] = @options[:tags].split(',') if @options[:tags]
+  end
+
+  def run
+    run_command(command_runner)
+    run_command(ANSIBLE_RUNNER) if command_runner == TERRAFORM_RUNNER && @options[:command] == APPLY_COMMAND
+  end
+
+  private
+
+  def command_runner
+    case @options[:command]
+    when APPLY_COMMAND, DESTROY_COMMAND
+      TERRAFORM_RUNNER
+    when RUN_COMMAND
+      ANSIBLE_RUNNER
     end
   end
-end.parse!
 
-options[:kube_id] ||= ENV['KUBE_ID']
-options[:web_ip] ||= ENV['WEB_IP']
-options[:worker_ip] ||= ENV['WORKER_IP']
-options[:token] ||= ENV['DO_TOKEN']
-options[:tags] = options[:tags].split(',') if options[:tags]
-
-def get_runner(command)
-  case command
-  when 'apply', 'destroy'
-    'terraform'
-  when 'run'
-    'ansible'
-  end
-end
-
-runner = get_runner(options[:command])
-Dir.chdir(runner) do
-  Object.const_get(runner.capitalize).run(options) do |command|
-    puts command
-    system(command)
-  end
-end
-
-if runner == 'terraform' && options[:command] == 'apply'
-  runner = 'ansible'
-  Dir.chdir(runner) do
-    Object.const_get(runner.capitalize).run(options) do |command|
-      puts command
-      system(command)
+  def run_command(runner)
+    Dir.chdir(runner) do
+      Object.const_get(runner.capitalize).run(@options) do |command|
+        puts command
+        system(command)
+      end
     end
   end
 end
+
+InfraCLI.new.run
--- bin/lib/ansible.rb
+++ bin/lib/ansible.rb
@@ -4,24 +4,18 @@ module Ansible
 
   def run(options)
     @options = options
-    @env = options[:module]
     @config = send(@options[:module])
-    unless @options[:module] == 'kube'
-      output_contents = File.read("./outputs/#{options[:module]}.json")
-      @inventory = JSON.parse(output_contents)['web_ip']['value']
-    end
+    @output = JSON.parse(File.read("./outputs/#{options[:module]}.json"))
+    @inventory = @output['web_ip']['value'] unless @output['web_ip'].nil?
     @tags = @options[:tags] || @config[:tags]
-    @vars = { env: @env }
     yield build_command
   end
 
   def build_command
     command = ['ansible-playbook', '-e @extra_vars.yml', '-e @custom_vars.yml']
     command << "--inventory #{@inventory},"
-    command << "--tags #{@tags.join(',')}" if @tags&.any?
-    @vars.each do |key, value|
-      command << "-e #{key}=#{value}"
-    end
+    command << "--tags #{@tags.join(',')}" if @tags
+    command << "-e env=#{@options[:module]}"
     command << @config[:playbook]
     command.join(' ')
   end
@@ -36,31 +30,20 @@ module Ansible
   def worker
     {
       playbook: 'setup_worker.yml',
-      tags: %w[setup server docker client],
-      ip_env: 'WORKER_IP',
-      vars: {
-        web_ip: @options[:web_ip]
-      }
+      tags: %w[setup server docker client]
     }
   end
 
   def ansible
     {
       playbook: 'setup_ansible.yml',
-      tags: %w[setup server docker server],
-      ip_env: 'ANSIBLE_IP',
-      vars: {
-        do_token: @options[:token],
-        web_ip: @options[:web_ip],
-        worker_ip: @options[:worker_ip]
-      }
+      tags: %w[setup server docker server]
     }
   end
 
   def jenkins
     {
-      playbook: 'setup_jenkins.yml',
-      ip_env: 'JENKINS_IP'
+      playbook: 'setup_jenkins.yml'
     }
   end
 
@@ -79,8 +62,7 @@ module Ansible
   def mercor
     {
       playbook: 'setup_mercor.yml',
-      tags: %w[setup server docker mercor],
-      ip_env: 'MERCOR_IP'
+      tags: %w[setup server docker mercor]
     }
   end
 end
