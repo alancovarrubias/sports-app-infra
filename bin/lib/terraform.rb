acb8e5f5b06f797c12294b6308fc130e21963cc9
Alan Covarrubias <aecovarru@gmail.com>
2025-12-09 08:29:55 -0800

    update terraform

--- bin/lib/terraform.rb
+++ bin/lib/terraform.rb
@@ -3,13 +3,18 @@ class Terraform
     @module = options[:module]
     @command = options[:command]
     @output_file = options[:output_file]
-    @target = @module == 'prod' ? '-target=module.infra' : ''
   end
 
   def run
     yield "terraform -chdir=#{@module} init"
-    yield "terraform -chdir=#{@module} #{@command} #{@target} -var-file=../terraform.tfvars --auto-approve"
-    yield "terraform -chdir=#{@module} output -json > #{@output_file}"
-    yield "terraform -chdir=#{@module} output -raw kubeconfig > ~/.kube/sports-app.yaml" if @module == 'prod'
+    if @module == 'prod'
+      yield "terraform -chdir=#{@module} #{@command} -target=module.infra -var-file=../terraform.tfvars --auto-approve"
+      yield "terraform -chdir=#{@module} #{@command} -target=module.k8s -var-file=../terraform.tfvars --auto-approve"
+      yield "terraform -chdir=#{@module} output -json > #{@output_file}"
+      yield "terraform -chdir=#{@module} output -raw kubeconfig > ~/.kube/sports-app.yaml"
+    else
+      yield "terraform -chdir=#{@module} #{@command} -var-file=../terraform.tfvars --auto-approve"
+      yield "terraform -chdir=#{@module} output -json > #{@output_file}"
+    end
   end
 end
