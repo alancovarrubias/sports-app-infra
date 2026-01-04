16f93fc117de854e4cae5e0daf45323ac40688dc
Alan Covarrubias <aecovarru@gmail.com>
2026-01-03 23:32:39 -0800

    Add ssl to prod

--- terraform/prod/modules/ingress/resources.tf
+++ terraform/prod/modules/ingress/resources.tf
@@ -24,3 +24,16 @@ data "kubernetes_service" "nginx_ingress" {
 
   depends_on = [helm_release.nginx_ingress]
 }
+
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
